afcd9b76515efcfb60dd07a68a86389169e1f1f6
chaimaa faris <chaimaafaris364@gmail.com>
2025-02-12 23:34:42 +0100

    add width and height for the image banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,6 @@ export default function Home() {
     <div className="container mx-auto flex items-center justify-between text-blue text-xl font-bold mt-[150px] leading-[1.20] font-poppins"><div>Bonjour👋,
       <p>Je suis</p>
       <p className="bg-text-gradient bg-clip-text text-transparent">Chaimaa Faris</p>
-      développeuse full-stack</div><Image src="bannerimage.svg" alt="bannerimage"></Image></div>
+      développeuse full-stack</div><Image src="bannerimage.svg" alt="bannerimage" width={349} height={349}></Image></div>
   );
 }
