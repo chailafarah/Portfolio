f378c845bbc9cff0ef23656c8c9a065b9a2d224c
chaimaa faris <chaimaafaris364@gmail.com>
2025-03-19 13:52:43 +0100

    Ajouter le margin

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export function Hero() {
   return (
-    <section className="md:flex md:flex-row-reverse md:items-center md:justify-between mt-8 lg:mt-24 mb-16">
+    <section className="md:flex md:flex-row-reverse md:items-center md:justify-between mt-8 lg:mt-24 mb-24">
       <Image className="mb-4 w-[116] md:w-[232] lg:w-[349]" src="/me.svg" alt="Photo de profil" loading="eager" width={349} height={349} />
 
       <h1 className="text-corn-flower-blue text-3xl md:text-4xl lg:text-6xl font-bold leading-[1.20]">
