db3001a0973773e64966aad1e4f3e7ccc249f37d
chaimaa faris <chaimaafaris364@gmail.com>
2025-03-21 22:06:50 +0100

    changer le width pour l'image de profile

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export function Hero() {
   return (
     <section className="md:flex md:flex-row-reverse md:items-center md:justify-between mt-8 lg:mt-24 mb-24">
-      <Image className="mb-4 w-[116] md:w-[232] lg:w-[349]" src="/me.svg" alt="Photo de profil" loading="eager" width={349} height={349} />
+      <Image className="mb-4 mx-auto mb-8 w-[200] md:w-[232] lg:w-[349]" src="/me.svg" alt="Photo de profil" loading="eager" width={349} height={349} />
 
       <h1 className="text-corn-flower-blue dark:text-white text-3xl md:text-4xl lg:text-6xl font-bold leading-[1.20]">
         Bonjour👋,<br/>
